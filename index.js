8907becfe43aa9a2a9eb2c69aaadfd5824b2f27e
drianlarde <aadabelarde2020@plm.edu.ph>
2023-04-11 12:00:29 +0800

    Update index.js

--- index.js
+++ index.js
@@ -24,10 +24,7 @@ const allowCors = (req, res, next) => {
   res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS,PATCH,DELETE,POST,PUT");
   res.setHeader(
     "Access-Control-Allow-Headers",
-    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-    "Origin",
-    "X-Api-Key",
-    "Authorization"
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Origin, X-Api-Key, Authorization"
   );
 
   if (req.method === "OPTIONS") {
